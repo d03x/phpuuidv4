1d7650f6b007e73e5c8e0919c1df3087366d5da9
dadanhdyt <dadanhidyt@gmail.com>
2024-06-05 20:14:09 +0700

    First❤❤❤

---
+++ src/uuidv4.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace dadanski\phpuuidv4;
+
+/**
+ * Undocumented function.
+ *
+ * @return void
+ */
+function uuidv4(): string
+{
+    $data = random_bytes(16);
+    $data[6] = chr(ord($data[6]) & 0x0F | 0x40);
+    $data[8] = chr(ord($data[8]) & 0x3F | 0x80);
+
+    return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
+}
+/**
+ * Undocumented function.
+ */
+function isUuId(string $uuid): bool
+{
+    return is_string($uuid) || (preg_match('/^[a-f\d]{8}(-[a-f\d]{4}){4}[a-f\d]{8}$/i', $uuid) !== 1);
+}
